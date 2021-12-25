51a9046544b85bd88b5e8c416fc87e5d84be188c
Yusuf Khalid <khalidalade2004@gmail.com>
2021-12-25 22:57:10 +0300

    added .env file and set SQL_TRACK_MODIFICATIONS to false

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS =  False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
